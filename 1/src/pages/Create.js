c07f151bcc7f0a0acc24f10ddbd78d06e74c28b9
jeraldpraveen <jerald.vit@gmail.com>
2022-05-23 00:03:21 +0530

    3.Typography

--- 1/src/pages/Create.js
+++ 1/src/pages/Create.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Create() {
-  return <div>Create page</div>;
+  return (
+    <div>
+      <Typography variant="h1" color="primary" align="center">
+        Create A New Note
+      </Typography>
+    </div>
+  );
 }
